a609dfa085d5f7c9f87891b40013408c1a8c6acc
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-16 16:32:30 +0530

    reverse functionality & dom selection

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
-console.log("connected");
+const inputEl = document.querySelector("[data-input-string]");
+const copyBtnEl = document.querySelector("[data-btn-copy]");
+
+function reverse(string) {
+  // convert to array
+  const strArr = string.split("");
+  // reverse the array
+  strArr.reverse();
+  // convert reverse array back to string
+  const revStr = strArr.join("");
+  return revStr;
+}
+
+function handleClick() {
+  const currInput = inputEl.value;
+}
+
+copyBtnEl.addEventListener("click", handleClick);
